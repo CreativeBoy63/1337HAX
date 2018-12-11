1742c4969197678625c0fce5ec96662cd4b7cab0
Isaac <Isaac.Schultz@wsu.edu>
2018-12-10 17:49:44 -0800

    massive data throughput

--- Pinger.cs
+++ Pinger.cs
@@ -14,7 +14,6 @@ namespace PingTest
         bool pinging;
         IPAddress ip;
         byte[] bytes;
-        Ping[] ping;
         Thread thread;
 
         public Pinger(string inputIP, byte[] b)
@@ -26,11 +25,11 @@ namespace PingTest
 
         public void StartPing()
         {
-            ping = new Ping[Environment.ProcessorCount];
+            Ping[] ping = new Ping[Environment.ProcessorCount];
             //ping = new Ping();
             pinging = true;
-            thread = new Thread(() => Parallel.ForEach(ping, b => Ping(b = new Ping()))); //runs a bunch of threads in parallel and creates a new ping object for each thread
-            //thread = new Thread(() => Bing(ping));
+            thread = new Thread(() => Parallel.ForEach(ping, b => Ping())); //runs a bunch of threads in parallel and creates a new ping object for each thread
+            //thread = new Thread(() => Ping());
             thread.Start();
         }
 
@@ -40,10 +39,25 @@ namespace PingTest
             thread.Join(); // waits until the thread stops
         }
 
-        private void Ping(Ping p)
+        private void Ping()
         {
             while (pinging) //pings forever until after pinging is set to false.
-                p.Send(ip, 1, bytes); 
+            {
+                //PingReply reply = p.Send(ip, 0, bytes);
+
+                MyPing ping = delegate ()
+                {
+                    var pingSender = new Ping();
+                    PingReply reply = pingSender.Send(ip, 1, bytes);
+                };
+                var asyncResults = new IAsyncResult[0x200];
+                for (int i = 1; i < 0x200; i++)
+                    asyncResults[i] = ping.BeginInvoke(null, null);
+                for (int i = 1; i < 0x200; i++)
+                    ping.EndInvoke(asyncResults[i]);
+            }
         }
+
+        private delegate void MyPing();
     }
 }
